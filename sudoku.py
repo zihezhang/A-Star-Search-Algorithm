399f25bf265d8c74aa66a80d5616ed974e015f51
Zihe Zhang <51214897+zihezhang@users.noreply.github.com>
2020-09-14 01:18:43 -0400

    Solve sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,20 +1,64 @@
 puzzle = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [2,0,9,0,0,0,6,0,0],
+    [0,4,0,8,7,0,0,1,2],
+    [8,0,0,0,1,9,0,4,0],
+    [0,3,0,7,0,0,8,0,1],
+    [0,6,5,0,0,8,0,3,0],
+    [1,0,0,0,3,0,0,0,7],
+    [0,0,0,6,5,0,7,0,9],
+    [6,0,4,0,0,0,0,2,0],
+    [0,8,0,3,0,1,4,5,0]
 ]
+
+
+def solve(board):
+    find = find_empty(board)
+    if not find: #if there are no empties
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if check(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+def check(board, number, position):
+    # Check box
+    x = position[1] // 3
+    y = position[0] // 3
+
+    for i in range(y*3, y*3 + 3):
+        for j in range(x * 3, x*3 + 3):
+            if board[i][j] == number and (i,j) != position:
+                return False
+    
+    # Check row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    
+
+    return True
 
 
 def print_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
+            print("- - - - - - - - - - - - ")
 
         for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
@@ -27,12 +71,26 @@ def print_board(board):
 
 
 def find_empty(board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
+    for i in range(len(board)): #loops through row
+        for j in range(len(board[0])): #loops through column
             if board[i][j] == 0:
-                return (i, j)  # row, col
+                return (i, j)  
 
     return None
 
+# a function to read the 
+def read_img():
+    # I wanted the user to have the liberty to choose the image
+    print("Enter image name: ")
+    image_url = input()
+    #image url also conatins the image extension eg. .jpg or .png
+    #reading in greayscale
+    img = cv2.imread(image_url, img = cv2.imread(image_url, cv2.IMREAD_GRAYSCALE))
+
 
 print_board(puzzle)
+solve(puzzle)
+print("________________________")
+print_board(puzzle)
+
+# Done with the help of https://techwithtim.net/tutorials/python-programming/sudoku-solver-backtracking/
